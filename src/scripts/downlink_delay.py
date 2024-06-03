fb032c1fcc702347d0f6e9139a7aaafaad07fa43
Achilleas Seisa <achilleas2942@gmail.com>
2024-06-03 23:54:52 +0200

    Add downlink delay

---
+++ src/scripts/downlink_delay.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import TwistStamped
+from std_msgs.msg import Float32
+
+
+class DownlinkDelay:
+    def __init__(self):
+        self.downlink = Float32()
+        self.downlink = 0.0
+        self.counter = 0
+
+        self.cmd_vel_sub = rospy.Subscriber(
+            "/pelican/vel_msg", TwistStamped, self.callback_cmd_vel
+        )
+        self.down_delay_pub = rospy.Publisher(
+            "/downlink_delay", Float32, queue_size=10
+        )
+
+
+    def callback_cmd_vel(self, msg):
+        recv_time = rospy.Time.now()
+        pub_time = msg.header.stamp
+        delay = (recv_time - pub_time).to_sec()
+        if self.counter < 500:  # sliding window of 500 messages
+            self.counter = self.counter + 1
+        self.downlink = np.divide(
+            delay + (self.counter - 1) * self.downlink, self.counter
+        )
+        self.down_delay_pub.publish(self.downlink)
+
+
+if __name__ == "__main__":
+    rospy.init_node("downlink_delay_node", anonymous=True)
+    node = DownlinkDelay()
+    rospy.spin()
