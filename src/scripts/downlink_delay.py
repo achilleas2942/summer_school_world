5dc3d4fc4497c4700b4d10ab706200cd29d198b2
Achilleas Seisa <achilleas2942@gmail.com>
2024-06-06 11:44:05 +0200

    Fix bug for delay to only be published when new messages are received

--- src/scripts/downlink_delay.py
+++ src/scripts/downlink_delay.py
@@ -11,6 +11,7 @@ class DownlinkDelay:
         self.downlink = Float32()
         self.downlink = 0.0
         self.counter = 0
+        self.old_header = 0.0
 
         self.cmd_vel_sub = rospy.Subscriber(
             "/pelican/vel_msg", TwistStamped, self.callback_cmd_vel
@@ -21,15 +22,21 @@ class DownlinkDelay:
 
 
     def callback_cmd_vel(self, msg):
-        recv_time = rospy.Time.now()
-        pub_time = msg.header.stamp
-        delay = (recv_time - pub_time).to_sec()
-        if self.counter < 500:  # sliding window of 500 messages
-            self.counter = self.counter + 1
-        self.downlink = np.divide(
-            delay + (self.counter - 1) * self.downlink, self.counter
-        )
-        self.down_delay_pub.publish(self.downlink)
+        if (
+            msg.header.stamp.to_sec() != 0
+            and msg.header.stamp.to_sec() != self.old_header
+        ):
+            recv_time = rospy.Time.now()
+            if recv_time.to_sec() != 0:
+                pub_time = msg.header.stamp
+                delay = (recv_time - pub_time).to_sec()
+                if self.counter < 500:  # sliding window of 500 messages
+                    self.counter = self.counter + 1
+                self.downlink = np.divide(
+                    delay + (self.counter - 1) * self.downlink, self.counter
+                )
+                self.old_header = msg.header.stamp.to_sec()
+                self.down_delay_pub.publish(self.downlink)
 
 
 if __name__ == "__main__":
